7df7a44cbcabd889311e00a7d39d09ee94d46e5f
zhangjian <zhangjian@xnhd.com>
2019-06-20 15:39:22 +0800

    add

---
+++ find_bf.py
@@ -0,0 +1,14 @@
+#!/bin/bash/env python
+#!-*- encoding=utf-8 -*-
+import os
+def FB(path):
+        for filepath,dirs,files in os.walk(path):
+                for f in files:
+                        pfile = os.path.join(filepath,f)
+                        fsize = os.path.getsize(pfile)
+                        fsize = round(float(fsize / (1024*1024)),2)
+                        if fsize > 20:
+                                print('Big File: ' + str(pfile) + ' ' + str(fsize) + 'MB')
+if __name__ == '__main__':
+        path = '/root/test'
+        FB(path)
