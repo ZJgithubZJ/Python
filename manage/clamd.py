2cad2116474de1dec6f1eb9f0d7f20f29b9fdfda
zhangjian <zhangjian@xnhd.com>
2019-10-31 13:53:37 +0800

    deploy

--- manage/clamd.py
+++ manage/clamd.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python
 #!-*- encoding=utf-8 -*-
 #Auther:ZJ
+'''
+客户端：
+1:yum install -y clamd clamav clamav-update
+2:chkconfig clamd on
+3:/usr/bin/freshclam
+4:setenforce 0
+5:sed -i -e '/^TCPAddr/ s/127.0.0.1/0.0.0.0/' /etc/clamd.conf
+6:/etc/init.d/clamd start
+
+主控端:
+1:wget http://xael.org/norman/python/pyclamd/pyClamd-0.3.15.tar.gz
+2:tar -zxvf pyClamd-0.3.15.tar.gz
+3:cd pyClamd-0.3.15
+4:python setup.py install
+'''
+
 import pyclamd
 class scan:
   def __init__(self,ip,port,path):
@@ -16,6 +32,10 @@ class scan:
         print('Connection Error!')
         return
       cd.reload()
+      '''
+      若检测对象为多个，可使用multiscan_file多线程扫描方法
+      print(cd.multiscan_file(self.path))
+      '''
       print(cd.contscan_file(self.path))
     except Exception as e:
       print(self.ip + str(e))
