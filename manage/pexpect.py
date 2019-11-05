bc575a27b59ccbeb17bf382557c29fec0cce7e9a
zhangjian <zhangjian@xnhd.com>
2019-11-05 19:43:16 +0800

    deploy

--- manage/pexpect.py
+++ manage/pexpect.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 #!-*- encoding=utf-8 -*-
 #Auther: ZJ
-#from __future__ import unicode_literals
+#from __future__ import unicode_literals	#这里不知道为什么要用unicode编码
 import sys
 import pexpect
 
 child = pexpect.spawnu('ftp 49.234.17.164 4449')
-child.expect('(?i)name .*:')
+child.expect('(?i)name .*:')			#(?i)表示忽略大小写匹配
 child.sendline('nginx')
 child.expect('(?i)password:')
 child.sendline('ftpuser')
@@ -15,12 +15,12 @@ child.sendline('bin')
 child.expect('ftp>')
 child.sendline('cd web4')
 child.expect('ftp>')
-child.sendline('put login_act109.jpg')
+child.sendline('put login_act109.jpg')		#定义操作
 child.expect('ftp>')
-sys.stdout.write(child.before)
+sys.stdout.write(child.before)			#打印最近一次匹配成功之前的内容
 print("Escape character is '^]'.\n")
-sys.stdout.write(child.after)
-sys.stdout.flush
-#child.interact()
+sys.stdout.write(child.after)			
+sys.stdout.flush				#刷新sys.stdout，在屏幕上看到实时输出
+#child.interact()				#让出控制权，退出脚本，进入到交互式界面
 child.sendline('quit')
 child.close()
