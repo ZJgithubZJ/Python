bf5ba12b7e8b980439aeeb78a2e7668f69ff839d
zhangjian <zhangjian@xnhd.com>
2019-11-06 17:07:01 +0800

    deploy

--- manage/ssh_client.py
+++ manage/ssh_client.py
@@ -2,6 +2,7 @@
 #!-*- encoding=utf-8 -*-
 #Auther: ZJ
 '''
+功能：实现通过秘钥登录多台机器执行指令
 此脚本要求本机要能够与iplist.txt里的所有主机无秘钥通信
 此处iplist.txt文件未列出
 '''
@@ -12,8 +13,8 @@ username = 'root'
 port = 22
 ssh = paramiko.SSHClient()		#创建ssh客户端对象
 key = paramiko.AutoAddPolicy()		#自动添加主机名以及秘钥到本地HostKey对象，不依赖load_system_host_keys()
-ssh.set_missing_host_key_policy(key)	#设置
-pkey = paramiko.RSAKey.from_private_key_file('/root/.ssh/id_rsa')
+ssh.set_missing_host_key_policy(key)	#设置远程主机没有本地主机的秘钥或HostKey对象时的策略
+pkey = paramiko.RSAKey.from_private_key_file('/root/.ssh/id_rsa')	#指定私钥文件
 
 def run():
   for ip in open('iplist.txt', 'r'):
