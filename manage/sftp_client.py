edbd95939ff9eb18d20d94ad9ebffe8e2a102883
zhangjian <zhangjian@xnhd.com>
2019-11-07 15:01:45 +0800

    deploy

--- manage/sftp_client.py
+++ manage/sftp_client.py
@@ -7,11 +7,11 @@ import sys
 username = 'root'
 hostname = '10.105.70.21'
 port = 22
-password = '51..dmz'
+password = ''
 
 try:
   t = paramiko.Transport((hostname,port))
-  t.connect(username=username,password=password)
+  t.connect(username=username,password=password)				#这里好像只能使用密码方式，没有秘钥方式
   sftp = paramiko.SFTPClient.from_transport(t)
   sftp.put("/root/test/syslogin.log","/root/syslogin.log")			#要具体到目标端的文件名
   sftp.get("/root/www.xx5.com.20190315.tgz","/root/www.xx5.com.20190315.tgz")	#要具体到本地端的文件名
