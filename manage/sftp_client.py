f68c99aefe8ad5ecd436566014b8f1b274629fe5
zhangjian <zhangjian@xnhd.com>
2019-11-07 14:57:15 +0800

    add

---
+++ manage/sftp_client.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#!-*- encoding=utf-8 -*-
+#Auther: ZJ
+import paramiko
+import sys
+
+username = 'root'
+hostname = '10.105.70.21'
+port = 22
+password = '51..dmz'
+
+try:
+  t = paramiko.Transport((hostname,port))
+  t.connect(username=username,password=password)
+  sftp = paramiko.SFTPClient.from_transport(t)
+  sftp.put("/root/test/syslogin.log","/root/syslogin.log")			#要具体到目标端的文件名
+  sftp.get("/root/www.xx5.com.20190315.tgz","/root/www.xx5.com.20190315.tgz")	#要具体到本地端的文件名
+  sftp.mkdir("/root/test",755)			#这是是三位数的权限，不能是0755，被坑了！
+  t.close()
+except Exception as e:
+  print(str(e))
