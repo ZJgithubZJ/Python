b1a4506386557ff562be35850d848ececc9a9fd5
zhangjian <zhangjian@xnhd.com>
2019-11-09 14:51:04 +0800

    deploy

--- manage/fabtask.sh
+++ manage/fabtask.sh
@@ -2,7 +2,8 @@
 for webip in `cat iplist.txt | awk '{print $2}'`
 do
 	name=`cat iplist.txt | grep $webip | awk '{print $1}'`
-	fab -H $webip webtask
+	#-H $webip默认是以当前登录用户登录的，如果你是非root用户执行指令，需要添加当前用户的公钥内容到目标主机认证，且这里需要修改为'-H root@$webip'
+	fab -H $webip webtask	
 	if [[ $? -eq 0 ]]; then
 		echo $name done!
 	else
