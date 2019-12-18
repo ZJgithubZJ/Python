811f272e88e44e66cf6439d682f005814a9b03be
zhangjian <zhangjian@xnhd.com>
2019-12-18 10:57:41 +0800

    deploy

--- monitor/监控报警/mail.py
+++ monitor/监控报警/mail.py
@@ -7,21 +7,21 @@ from email.mime.text import MIMEText
 from email.header import Header
 
 class Sendmail:
-	def __init__(self,message):
+	def __init__(self):
 		#真实发信人
-		self.sender = 'zhangjian@xnhd.com'
+		self.sender = 'mail01'
 		#真实收信人
 		self.receiver = 'zhangjian@xnhd.com'
-		#self.message = MIMEText('Python 邮件报警测试-1','plain','utf-8')
-		self.message = message
+		self.message = MIMEText('Python mail01.storyawine 测试邮件','plain','utf-8')
+		#self.message = message
 		#账面显示的From、To，真实是由self.sender代发。
-		self.message['From'] = Header('Proxy_mail','utf-8')
-		self.message['To'] = Header('ZJ','utf-8')
+		self.message['From'] = Header('75-65','utf-8')
+		self.message['To'] = Header('zhangjian','utf-8')
 		self.message['Subject'] = Header('Python SMTP test','utf-8')
 	def sendmail(self):
 		try:
-			server = smtplib.SMTP_SSL('smtp.exmail.qq.com',465)
-			server.login('zhangjian@xnhd.com','mail passwd')
+			server = smtplib.SMTP('mail01.storyawine.com')
+			#server.login('zhangjian@xnhd.com','mail passwd')
 			server.sendmail(self.sender,self.receiver,self.message.as_string())
 			print('Mail send succeed!')
 			server.quit()
